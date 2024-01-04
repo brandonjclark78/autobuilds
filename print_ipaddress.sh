8b7517e9c5b52067439aaf1dc3d4940caf3919d0
brandonjclark78 <brandonjclark78@gmail.com>
2024-01-04 01:09:44 -0600

    fixing green text

--- print_ipaddress.sh
+++ print_ipaddress.sh
@@ -3,4 +3,4 @@ green='\e[32m'  # Green text
 reset='\e[0m'   # Reset text color to default
 ipaddr=$(hostname -I)
 hostname=$(hostname)
-echo -e "The IP Address of $hostname is: ${green}$ipaddr"
+echo -e "The IP Address of $hostname is: ${green}$ipaddr ${reset}"
