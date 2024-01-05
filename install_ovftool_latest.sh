7a005696431947958ad0b16a3d12fccc61fdf1fa
brandonjclark78 <brandonjclark78@gmail.com>
2024-01-05 17:27:15 -0600

    adding some text

--- install_ovftool_latest.sh
+++ install_ovftool_latest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#https://mikebosland.com/installing-ovftool/ < thanks mike
+
 #download latest version of vmware OVFtool to /shared
 curl -sSL -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" "https://developer.vmware.com/web/tool/ovf-tool/#latest" | grep -oP 'href="\K[^"]*(ovftool.*\-lin.x86_64.zip)[^"]*' | xargs -I {} wget {} -P /shared
 
